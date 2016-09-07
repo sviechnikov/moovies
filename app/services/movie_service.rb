df0f16098948025efa1926aedd045ed9d5faa24c
Maksym Sviechnikov <maksym.sviechnikov@litslink.com>
2016-09-07 10:49:52 +0300

    preparing to pluralizing of other documents

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,5 +1,16 @@
 class MovieService
-  def self.all
-    JSON.parse RestClient.get 'http://api.androidhive.info/json/movies.json'
+  API_HOST = 'http://private-20bfe-themoviedb.apiary-mock.com/3/movie/'.freeze
+
+  def self.popular
+  end
+
+  private
+
+  def req
+    uest(path)
+    uri = URI::HTTP.build(host: API_HOST, path: path, api_key: Rails.application.secrets.apiary_api_key)
+
+    response = RestClient.get(uri)
+    JSON.parse response
   end
 end
